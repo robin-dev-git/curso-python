8113dfd2700952f2bf17cd720b08ef02e77afbcd
robin-dev-git <robin-0507@outlook.com>
2022-02-15 18:35:37 +0000

    Agrega varios archivos y carpetas

---
+++ parte02/2.1_listas_comprension.py
@@ -0,0 +1,56 @@
+numeros = [8, 2, 3, 5, 12, 10, 13, 19, 7, 11, 6, 1]
+
+print(type(numeros))
+print(len(numeros))
+print(numeros)
+
+print('-'*50)
+
+pares_triplicados = []
+
+for n in numeros:
+    if n % 2 == 0:
+        pares_triplicados.append(n * 3)
+
+print(type(pares_triplicados))
+print(len(pares_triplicados))
+print(pares_triplicados)
+
+print('-'*50)
+
+pares_triplicados = [n * 3 for n in numeros if n % 2 == 0]
+
+print(type(pares_triplicados))
+print(len(pares_triplicados))
+print(pares_triplicados)
+
+print('-'*50)
+
+# Solución con ciclos for (iterativa):
+numeros_1 = [2, 3, 5, 7, 11]
+numeros_2 = [2, 7, 13, 19, 23, 3]
+interseccion = []
+
+for  n1 in numeros_1:
+    for n2 in numeros_2:
+        if n1 == n2 and n1 not in interseccion:
+            interseccion.append(n1)
+
+print(len(interseccion))
+print(interseccion)
+
+print('-'*50)
+
+# Solución con listas comprensión:
+interseccion = []
+resultado = [n1 for n2 in numeros_2 for n1 in numeros_1 if n1 == n2 and n1 not in interseccion]
+
+print(len(resultado))
+print(resultado)
+
+print('-'*50)
+
+resultado = set(numeros_1) & set(numeros_2)
+print(len(resultado))
+print(resultado)
+
---
+++ parte03/3.1_medicion_tiempo_ejecucion.py
@@ -0,0 +1,48 @@
+from timeit import timeit
+
+inicializacion = 'from math import sqrt'
+codigo = '''
+raices = []
+for i in range(100):
+    raices.append(sqrt(i))
+'''
+
+print(timeit(setup=inicializacion, stmt=codigo, number=10000))
+
+codigo = """
+raices = []
+raices = [lambda i: sqrt(i) for i in range(100)]
+"""
+
+print(timeit(setup=inicializacion, stmt=codigo, number=10000))
+
+print('-'*80)
+
+# Comparación algoritmos de ordenamiento: Selección VS. Inserción
+
+codigo_seleccion = """
+A = [64, 25, 12, 22, 11]
+
+for i in range(len(A)):
+    min_idx = i
+    for j in range(i + 1, len(A)):
+        if A[min_idx] > A[j]:
+            min_idx = j
+    A[i], A[min_idx] = A[min_idx], A[i]
+"""
+
+codigo_insercion = """
+arr = [64, 25, 12, 22, 11]
+for i in range(1, len(arr)):
+
+    key = arr[i]
+
+    j = i - 1
+    while j >= 0 and key < arr[j]:
+        arr[j+1] = arr[j]
+        j -= 1
+    arr[j+1] = key
+"""
+
+print(timeit(stmt=codigo_seleccion, number=1000000))
+print(timeit(stmt=codigo_insercion, number=1000000))
